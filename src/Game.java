a0a3523fe5bdb206dd2cd89741131ef687b296b7
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-10-02 22:35:46 -0300

    load and save games to dataabse

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Game {
@@ -13,6 +16,11 @@ public class Game {
 		this.title = p_title;
 	}
 	
+	Game(String p_id, String p_title) {
+		this.id = UUID.fromString(p_id);
+		this.title = p_title;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -23,8 +31,20 @@ public class Game {
 		return listPrice;
 	}
 	
+	public void setListPrice(int p_listPrice) {
+		this.listPrice = p_listPrice;
+	}
+	
+	public void setSalePrice(int p_salePrice) {
+		this.salePrice = p_salePrice;
+	}
+	
 	public String getCoverImageUrl() {
 		return coverImage;
+	}
+	
+	public void setCoverImage(String p_coverImage) {
+		this.coverImage = p_coverImage;
 	}
 
 	public UUID getId() {
@@ -44,7 +64,20 @@ public class Game {
 	}
 	
 	public void save(DataBase dataBase) {
+		String query = "INSERT INTO games (id, title, cover_image, list_price, sale_price, is_active)";
 		
+		query = query + " VALUES (";
+		query = query + "'" + this.id.toString() + "'" + ", ";
+		query = query + "'" + this.title + "'" + ", ";
+		query = query + "'" + this.coverImage + "'" + ", ";
+		query = query + this.listPrice + ", ";
+		query = query + this.salePrice + ", ";
+		query = query + this.isActive;
+		query = query + ");";
+		
+		//System.out.println(query);
+		
+		dataBase.execute(query);
 	}
 	
 	public void update(DataBase dataBase) {
@@ -53,5 +86,40 @@ public class Game {
 	
 	public void delete(DataBase dataBase) {
 		
+	}
+	
+	public static ArrayList<Game> loadGames(DataBase dataBase, String targetProperty, String targetValue) {
+		ResultSet rawGames;
+		String query = "SELECT * FROM Games";
+		ArrayList<Game> games = new ArrayList<Game>();
+		
+		if(targetProperty.isEmpty() == false && targetValue.isEmpty() == false) {
+			query = query + "WHERE " + targetProperty + " = " + targetValue + ";";
+		}
+		
+		rawGames = dataBase.get(query);
+		
+		try {
+			while (rawGames.next()) {
+				String id = rawGames.getString(1);
+				String title = rawGames.getString(2);
+				String coverImage = rawGames.getString(3);
+				Integer listPrice = rawGames.getInt(4);
+				Integer salePrice = rawGames.getInt(5);
+				Boolean isActive = rawGames.getBoolean(6);
+				Game game = new Game(id, title);
+				
+				game.setCoverImage(coverImage);
+				game.setListPrice(listPrice);
+				game.setSalePrice(salePrice);
+				game.setActive(isActive);
+				
+				games.add(game);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return games;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,9 +11,20 @@ public class Main {
 		dataBase.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";");
 		
 		// create tables
-		dataBase.execute("CREATE TABLE games ( id UUID NOT NULL PRIMARY KEY, title VARCHAR(50) NOT NULL, cover_image VARCHAR(100) NOT NULL, list_price INT NOT NULL, sale_price INT NOT NULL, is_active BOOLEAN NOT NULL );");
-		dataBase.execute("CREATE TABLE users ( id UUID NOT NULL PRIMARY KEY, first_name VARCHAR(10) NOT NULL, last_name VARCHAR(10) NOT NULL, wallet INT NOT NULL, points INT NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, creation_date DATE NOT NULL );");
-		dataBase.execute("CREATE TABLE user_games ( user_id UUID NOT NULL REFERENCES users(id), game_id UUID NOT NULL REFERENCES games(id), PRIMARY KEY (user_id, game_id) );");
+		dataBase.execute("CREATE TABLE IF NOT EXISTS games ( id UUID NOT NULL PRIMARY KEY, title VARCHAR(50) NOT NULL, cover_image VARCHAR(100) NOT NULL, list_price INT NOT NULL, sale_price INT NOT NULL, is_active BOOLEAN NOT NULL );");
+		dataBase.execute("CREATE TABLE IF NOT EXISTS users ( id UUID NOT NULL PRIMARY KEY, first_name VARCHAR(10) NOT NULL, last_name VARCHAR(10) NOT NULL, wallet INT NOT NULL, points INT NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, creation_date DATE NOT NULL );");
+		dataBase.execute("CREATE TABLE IF NOT EXISTS user_games ( user_id UUID NOT NULL REFERENCES users(id), game_id UUID NOT NULL REFERENCES games(id), PRIMARY KEY (user_id, game_id) );");
+		
+		/*Game testGame = new Game("Test Title");
+		
+		testGame.setListPrice(70);
+		testGame.setSalePrice(59);
+		
+		testGame.save(dataBase);*/
+		
+		ArrayList<Game> games = Game.loadGames(dataBase, "", "");
+		
+		System.out.println(games);
 		
 		dataBase.disconnect();
 	}
