7fa031bb38663a18a374cbedaadca02b80011456
Matheus H. Potrich <matheushp.com@hotmail.com>
2026-05-24 20:36:20 -0300

    feat: added create and delete routes for media

--- src/media/MediaController.java
+++ src/media/MediaController.java
@@ -2,9 +2,11 @@ package media;
 
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +31,19 @@ public class MediaController {
 	}
 	
 	@PostMapping("")
-	public String createMedia() {
+	public String createMedia(@RequestBody String p_body) {
+		Media newMedia = new Gson().fromJson(p_body, Media.class);
 		
+		newMedia.save(dataBase);
+		
+		return new Gson().toJson(newMedia, Game.class);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteGame(@PathVariable("id") UUID p_id) {
+		Media media = new Media();
+		
+		media.load(dataBase, p_id);
+		media.delete(dataBase);
 	}
 }
